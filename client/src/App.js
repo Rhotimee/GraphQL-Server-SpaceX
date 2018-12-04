19d78be576841881ce26f6e9e58d768aa3c0d0da
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2018-12-04 20:54:30 +0100

    connect react with the graphql server and list the launches

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'
+import Launches from './components/Launches'
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="container">
+          <img 
+            src="https://raw.githubusercontent.com/bradtraversy/spacex_launch_stats/master/client/src/logo.png" 
+            alt="SpaceX"
+            style={{ 
+              width: 300,
+              display: 'block', 
+              margin: 'auto'}}
+            />
+          <Launches />
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
+import cors from 'cors';
 import schema from './schema'
 
+
 const app = express();
+
+// Allow cros origin
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
   schema,
